3cdb058392dad4090b023faacf1fe4049f08c4b7
Rashed Abdel-Tawab <rashed@linux.com>
2018-06-10 01:51:14 +0000

    soong_config: Add TARGET_USES_QTI_CAMERA_DEVICE
    Change-Id: I925b137edeed9fa11bbe566002147b444328c661

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -9,10 +9,15 @@ type Product_variables struct {
 	Target_uses_color_metadata struct {
 		Cppflags []string
 	}
+	Uses_qti_camera_device struct {
+		Cppflags []string
+		Shared_libs []string
+	}
 }
 
 type ProductVariables struct {
 	Target_process_sdk_version_override *string `json:",omitempty"`
 	Target_shim_libs  *string `json:",omitempty"`
 	Target_uses_color_metadata  *bool `json:",omitempty"`
+ 	Uses_qti_camera_device  *bool `json:",omitempty"`
 }
--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -3,9 +3,10 @@ add_json_str_omitempty = $(if $(strip $(2)),$(call add_json_str, $(1), $(2)))
 _contents := $(_contents)    "Wave":{$(newline)
 
 # See build/core/soong_config.mk for the add_json_* functions you can use here.
- $(call add_json_str_omitempty, Target_process_sdk_version_override,    $(TARGET_PROCESS_SDK_VERSION_OVERRIDE))
+$(call add_json_str_omitempty, Target_process_sdk_version_override,    $(TARGET_PROCESS_SDK_VERSION_OVERRIDE))
 $(call add_json_str,  Target_shim_libs,                      $(TARGET_LD_SHIM_LIBS))
 $(call add_json_bool, Target_uses_color_metadata,            $(filter true,$(TARGET_USES_COLOR_METADATA)))
+$(call add_json_bool, Uses_qti_camera_device,                          $(filter true,$(TARGET_USES_QTI_CAMERA_DEVICE)))
 
 # This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
 _contents := $(_contents)__SV_END
