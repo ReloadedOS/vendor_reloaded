fe280bab9b7beb724047baf28be3613be8b4619e
Christian Oder <myself5@carbonrom.org>
2018-10-01 16:58:03 -0500

    extensions: frameworks: Add SecureSettingsListPreference
    * Also add the required SecureSettingsStore along with that
    
    Change-Id: I48f0d0757cd1439b34079a14f6229ba9b1aa2aa4

--- extensions/framework/src/me/waveproject/framework/preference/SecureSettingsStore.java
+++ extensions/framework/src/me/waveproject/framework/preference/SecureSettingsStore.java
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2020 Android Ice Cold Project
- *                    CarbonROM
+ * Copyright (C) 2017 AICP
+ * Copyright (C) 2018 CarbonROM
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
