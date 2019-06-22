adithya2306/platform_vendor_fred

envsetup: Run hidl-gen update script for mka & brunch as well
Change-Id: If0771df976d7c338ef1aeec8c23416d513601631

build/envsetup.sh
@@ -10,6 +10,7 @@ EOF
 function brunch()
 {
     breakfast $*
+
     if [ $? -eq 0 ]; then
         mka bacon
     else
@@ -65,6 +66,32 @@ function cout()
 
 # Make using all available CPUs
 function mka() {
+    if [ -f $ANDROID_BUILD_TOP/$QTI_BUILDTOOLS_DIR/build/update-vendor-hal-makefiles.sh ]; then
+        vendor_hal_script=$ANDROID_BUILD_TOP/$QTI_BUILDTOOLS_DIR/build/update-vendor-hal-makefiles.sh
+        source $vendor_hal_script --check
+        regen_needed=$?
+    else
+        vendor_hal_script=$ANDROID_BUILD_TOP/vendor/fred/scripts/vendor_hal_makefile_generator.sh
+        regen_needed=1
+    fi
+
+    if [ $regen_needed -eq 1 ]; then
+        _wrap_build $(get_make_command hidl-gen) hidl-gen ALLOW_MISSING_DEPENDENCIES=true
+        RET=$?
+        if [ $RET -ne 0 ]; then
+            echo -n "${color_failed}#### hidl-gen compilation failed, check above errors"
+            echo " ####${color_reset}"
+            return $RET
+        fi
+        source $vendor_hal_script
+        RET=$?
+        if [ $RET -ne 0 ]; then
+            echo -n "${color_failed}#### HAL file .bp generation failed dure to incpomaptible HAL files , please check above error log"
+            echo " ####${color_reset}"
+            return $RET
+        fi
+    fi
+
     m -j "$@"
 }
 

ReloadedOS/vendor_reloaded

build/envsetup.sh
@@ -7,14 +7,33 @@ EOF
 
 # Make using all available CPUs
 function mka() {
-    case `uname -s` in
-        Darwin)
-            make -j `sysctl hw.ncpu|cut -d" " -f2` "$@"
-            ;;
-        *)
-            schedtool -B -n 1 -e ionice -n 1 make -j `cat /proc/cpuinfo | grep "^processor" | wc -l` "$@"
-            ;;
-    esac
+    if [ -f $ANDROID_BUILD_TOP/$QTI_BUILDTOOLS_DIR/build/update-vendor-hal-makefiles.sh ]; then
+        vendor_hal_script=$ANDROID_BUILD_TOP/$QTI_BUILDTOOLS_DIR/build/update-vendor-hal-makefiles.sh
+        source $vendor_hal_script --check
+        regen_needed=$?
+    else
+        vendor_hal_script=$ANDROID_BUILD_TOP/vendor/wave/scripts/vendor_hal_makefile_generator.sh
+        regen_needed=1
+    fi
+
+    if [ $regen_needed -eq 1 ]; then
+        _wrap_build $(get_make_command hidl-gen) hidl-gen ALLOW_MISSING_DEPENDENCIES=true
+        RET=$?
+        if [ $RET -ne 0 ]; then
+            echo -n "${color_failed}#### hidl-gen compilation failed, check above errors"
+            echo " ####${color_reset}"
+            return $RET
+        fi
+        source $vendor_hal_script
+        RET=$?
+        if [ $RET -ne 0 ]; then
+            echo -n "${color_failed}#### HAL file .bp generation failed dure to incpomaptible HAL files , please check above error log"
+            echo " ####${color_reset}"
+            return $RET
+        fi
+    fi
+
+    m -j "$@"
 }
 
 function breakfast()