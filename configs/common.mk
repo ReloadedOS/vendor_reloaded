adithya2306/platform_vendor_fred

prebuilt: Add Pixel's sysconfigs
Also includes:

From: arian <arian.kulmer@web.de>
Date: Fri, 9 Nov 2018 10:45:02 +0100

 remove fake Pixel features

 this removes the pixel unlimited
 storage entry in Google Photos because it isn't working on non Pixel devices.
 It checks for the mac and detects it as non-pixel device. IT results in a
 false entry in photos app and won't allow the user to decrease the upload
 quality and fills the Google Drive Storage Change-Id:
 I59893308efa357d25bdd3d53e31904e90c4d84cb

config/common.mk
@@ -70,6 +70,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     ViaBrowser
 
+# Pixel sysconfig
+PRODUCT_COPY_FILES += \
+    vendor/fred/prebuilt/etc/sysconfig/pixel.xml:system/etc/sysconfig/pixel.xml
+
 # Fix Google Dialer
 PRODUCT_COPY_FILES +=  \
     vendor/fred/prebuilt/etc/sysconfig/dialer_experience.xml:system/etc/sysconfig/dialer_experience.xml

ReloadedOS/vendor_reloaded

configs/common.mk
@@ -149,3 +149,7 @@ PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
 # Fix Google Dialer
 PRODUCT_COPY_FILES +=  \
     vendor/wave/prebuilt/etc/sysconfig/dialer_experience.xml:system/etc/sysconfig/dialer_experience.xml
+
+# Pixel sysconfig
+PRODUCT_COPY_FILES += \
+    vendor/wave/prebuilt/etc/sysconfig/pixel.xml:system/etc/sysconfig/pixel.xml