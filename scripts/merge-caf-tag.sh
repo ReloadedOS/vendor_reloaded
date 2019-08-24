889151334df718d8d3f2c8368da00b29bc53e054
Adithya R <gh0strider.2k18.reborn@gmail.com>
2019-08-24 14:09:21 +0530

    scripts: All-new script to merge a CAF tag
    * Also auto-merges manifest and updates CAF version in vendor/wave
    * Fixed CAF's messed up naming of qcom/opensource and commonsys repos

--- scripts/merge-caf-tag.sh
+++ scripts/merge-caf-tag.sh
@@ -1,89 +1,125 @@
 #!/usr/bin/env bash
+# Script to merge latest CAF tag in WaveOS sources
+# Run it in the root of ROM source
 
-#COLORS -
+# Colors
 red=$'\e[1;31m'
 grn=$'\e[1;32m'
 blu=$'\e[1;34m'
 end=$'\e[0m'
 
-# Assumes source is in users home in a directory called "wave"
-WAVE_PATH="${HOME}/wave"
+# Assumes user is running the script in root of source
+WAVE_PATH=$(pwd)
+BRANCH=p
 
-# Set the tag you want to merge
-read -p "Enter the CAF tag you want to merge : " TAG
+# Get merge tag from user
+read -p "Enter the CAF tag you want to merge: " TAG
 
 # Set the base URL for all repositories to be pulled from
-CAF="https://source.codeaurora.org"
+CAF="https://source.codeaurora.org/quic/la"
 
-do_not_merge="device/qcom/common external/exfat manifest vendor/codeaurora/commonsys/telephony vendor/qcom/opensource/commonsys/bluetooth vendor/qcom/opensource/commonsys/bluetooth_ext vendor/qcom/opensource/commonsys/fm vendor/qcom/opensource/commonsys/system/bt vendor/qcom/opensource/commonsys-intf/display vendor/wave"
+blacklist="device/qcom/common \
+    manifest \
+    hardware/qcom/keymaster \
+    vendor/wave \
+    packages/apps/MusicFX \
+    vendor/qcom/opensource/power"
 
-# wave manifest is setup with repo name first, then repo path, so the path attribute is after 3 spaces, and the path itself within "" in it
-repos="$(grep 'remote="wave"' ${WAVE_PATH}/.repo/manifests/wave.xml  | awk '{print $2}' | awk -F '"' '{print $2}')"
+reset_branch () {
+  git checkout $BRANCH &> /dev/null
+  git fetch wave $BRANCH &> /dev/null
+  git reset --hard wave/$BRANCH &> /dev/null
+}
 
-cd ${WAVE_PATH}
+repos="$(grep 'remote="wave"' $WAVE_PATH/.repo/manifests/wave.xml  | awk '{print $2}' | awk -F '"' '{print $2}')"
 
-for filess in failed success notcaf unchanged; do
-    rm $filess 2> /dev/null
-    touch $filess
+for files in failed success notcaf unchanged; do
+    rm $files 2> /dev/null
+    touch $files
 done
 
-for REPO in ${repos}; do
-    echo -e ""
-    if [[ "${do_not_merge}" =~ "${REPO}" ]]; then
-        echo -e "${REPO} is not to be merged"
+for REPO in $repos; do
+    if [[ $blacklist =~ $REPO ]]; then
+        echo -e "\n$REPO is in blacklist, skipping"
     else
-        echo "$blu Merging $REPO $end"
-        echo -e ""
-        cd $REPO
-        git checkout p
-        git fetch wave p
-        git reset --hard wave/p
-        git remote rm caf 2> /dev/null
-        if [[ "${REPO}" == "device/qcom/sepolicy" ]]; then
-            reponame="${CAF}/${REPO}"
-        elif [[ "${REPO}" == "build/make" ]]; then
-            reponame="${CAF}/platform/build"
-        elif [[ "${REPO}" =~ "vendor/qcom" ]]; then
-            reponame="${CAF}/platform/$(echo ${REPO} | sed -e 's|qcom/opensource|qcom-opensource|')"
-        else
-            reponame="${CAF}/platform/$REPO"
-        fi
-        git remote add caf "${reponame}"
-        git fetch caf --quiet --tags
-        if [ $? -ne 0 ]; then
-            echo "$repos" >> ${WAVE_PATH}/notcaf
-        else
-            git merge ${TAG} --no-edit
-            if [ $? -ne 0 ]; then
-                echo "$REPO" >> ${WAVE_PATH}/failed
-                echo "$red $REPO failed :( $end"
-            else
-                if [[ "$(git rev-parse HEAD)" != "$(git rev-parse wave/p)" ]]; then
-                    echo "$REPO" >> ${WAVE_PATH}/success
-                    echo "$grn $REPO succeeded $end"
+        case $REPO in
+            device/qcom/sepolicy) repo_url="$CAF/$REPO" ;;
+            build/make) repo_url="$CAF/platform/build" ;;
+            vendor/codeaurora/commonsys/telephony)
+                repo_url="$CAF/platform/vendor/codeaurora/telephony" ;;
+            vendor/qcom/opensource/commonsys/bluetooth)
+                repo_url="$CAF/platform/vendor/qcom-opensource/bluetooth" ;;
+            vendor/qcom/opensource/commonsys/bluetooth_ext)
+                repo_url="$CAF/platform/vendor/qcom-opensource/bluetooth_ext" ;;
+            vendor/qcom/opensource/commonsys/fm)
+                repo_url="$CAF/platform/vendor/qcom-opensource/fm-commonsys" ;;
+            vendor/qcom/opensource/commonsys/system/bt)
+                repo_url="$CAF/platform/vendor/qcom-opensource/system/bt" ;;
+            vendor/qcom/opensource/commonsys-intf/display)
+                repo_url="$CAF/platform/vendor/qcom-opensource/display-commonsys-intf" ;;
+            vendor/qcom*) repo_url="$CAF/platform/$(echo $REPO | sed -e 's|qcom/opensource|qcom-opensource|')" ;;
+            *) repo_url="$CAF/platform/$REPO" ;; esac
+
+        if wget -q --spider $repo_url; then
+            echo -e "$blu \nMerging $REPO $end"
+            cd $REPO
+            reset_branch
+            git fetch -q $repo_url $TAG &> /dev/null
+            if git merge FETCH_HEAD -q -m "Merge tag '$TAG' into $BRANCH" &> /dev/null; then
+                if [[ $(git rev-parse HEAD) != $(git rev-parse wave/$BRANCH) ]] && [[ $(git diff HEAD wave/$BRANCH) ]]; then
+                    echo "$REPO" >> $WAVE_PATH/success
+                    echo "${grn}Merging $REPO succeeded :) $end"
                 else
                     echo "$REPO - unchanged"
+                    git reset --hard wave/$BRANCH &> /dev/null
                 fi
+            else
+                echo "$REPO" >> $WAVE_PATH/failed
+                echo "${red}$REPO merging failed :( $end"
             fi
-            echo -e ""
-        fi
-        cd ${WAVE_PATH}
+        else echo $REPO >> ${WAVE_PATH}/notcaf; fi
+        cd $WAVE_PATH
     fi
 done
 
-echo -e ""
-echo -e "$red These REPO failed $end"
-cat ./failed
-echo -e ""
-echo -e "$grn These succeeded $end"
-cat ./success
+echo -e "$red \nThese repos failed merging: \n $end"
+cat failed
+echo -e "$grn \nThese repos succeeded merging: \n $end"
+cat success
 
-# What's got succeed
-SUCCEED="$(cat success)"
+echo $red
+read -p "Do you want to push the succesfully merged repos? (Y/N): " PUSH
+echo $end
 
-# Push them
-for succeed in ${SUCCEED}; do
-    cd $succeed
-    git push $(git remote -v | grep wave | head -1 | awk '{print $2}') HEAD:p
-    cd -
-done
+if [[ $PUSH == "Y" ]] || [[ $PUSH == "y" ]]; then
+    # Push succesfully merged repos
+    for REPO in $(cat success); do
+        cd $REPO
+        echo -e "\nPushing $REPO ..."
+        git push -q wave HEAD:$BRANCH &> /dev/null
+        cd $WAVE_PATH
+    done
+
+    # Auto-merge manifest too
+    cd manifest
+    echo -e "\nMerging and pushing manifest ..."
+    reset_branch
+    wget -q "https://source.codeaurora.org/quic/la/platform/manifest/plain/${TAG}.xml?h=$TAG" -O default.xml &> /dev/null
+    sed -i '0,/default/s/default/default sync-j="6" sync-c="true"/' default.xml
+    sed -i '$i \ \ <include name="remove.xml" />' default.xml
+    sed -i '$i \ \ <include name="wave.xml" />' default.xml
+    git commit -a -m "manifest: Merge tag '$TAG' into $BRANCH" &> /dev/null
+    git push wave HEAD:$BRANCH &> /dev/null
+    cd $WAVE_PATH
+
+    # Update CAF version in vendor/wave
+    cd vendor/wave
+    echo -e "\nUpdating CAF version in vendor/wave and pushing ..."
+    reset_branch
+    sed -i "s|CAF_VERSION := .*|CAF_VERSION := $TAG|g" configs/branding.mk
+    git commit -a -m "configs: Update CAF version to $TAG" &> /dev/null
+    git push wave HEAD:$BRANCH &> /dev/null
+    cd $WAVE_PATH
+fi
+
+echo -e "\n${blu}All done :) $end"
