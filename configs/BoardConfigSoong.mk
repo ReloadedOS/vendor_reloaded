e8cefeed1f27bc99873890b30094a90224c6fd68
Pig <pig.priv@gmail.com>
2020-10-15 02:58:37 +0800

    soong: Add wave soong config variables
    Ref: https://android.googlesource.com/platform/build/soong/+/refs/tags/android-11.0.0_r4#soong-config-variables
    
    Change-Id: Icaf35b65b63d4245c92e39ce628bb8d6ffd0d4bb
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- configs/BoardConfigSoong.mk
+++ configs/BoardConfigSoong.mk
@@ -25,3 +25,39 @@ define addVar
 endef
 
 $(foreach v,$(EXPORT_TO_SOONG),$(eval $(call addVar,$(v))))
+
+SOONG_CONFIG_NAMESPACES += waveGlobalVars
+SOONG_CONFIG_waveGlobalVars += \
+    additional_gralloc_10_usage_bits \
+    target_init_vendor_lib \
+    target_ld_shim_libs \
+    target_process_sdk_version_override
+
+SOONG_CONFIG_NAMESPACES += waveQcomVars
+SOONG_CONFIG_waveQcomVars += \
+    supports_extended_compress_format \
+    uses_pre_uplink_features_netmgrd \
+    uses_qti_camera_device
+
+# Only create soong_namespace var if dealing with UM platforms to avoid breaking build for all other platforms
+ifneq ($(filter $(UM_PLATFORMS),$(TARGET_BOARD_PLATFORM)),)
+SOONG_CONFIG_waveQcomVars += \
+    qcom_soong_namespace
+endif
+
+# Soong bool variables
+SOONG_CONFIG_waveQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
+SOONG_CONFIG_waveQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
+SOONG_CONFIG_waveQcomVars_uses_qti_camera_device := $(TARGET_USES_QTI_CAMERA_DEVICE)
+
+# Set default values
+TARGET_INIT_VENDOR_LIB ?= vendor_init
+
+# Soong value variables
+SOONG_CONFIG_waveGlobalVars_additional_gralloc_10_usage_bits := $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS)
+SOONG_CONFIG_waveGlobalVars_target_init_vendor_lib := $(TARGET_INIT_VENDOR_LIB)
+SOONG_CONFIG_waveGlobalVars_target_ld_shim_libs := $(subst $(space),:,$(TARGET_LD_SHIM_LIBS))
+SOONG_CONFIG_waveGlobalVars_target_process_sdk_version_override := $(TARGET_PROCESS_SDK_VERSION_OVERRIDE)
+ifneq ($(filter $(UM_PLATFORMS),$(TARGET_BOARD_PLATFORM)),)
+SOONG_CONFIG_waveQcomVars_qcom_soong_namespace := $(QCOM_SOONG_NAMESPACE)
+endif
