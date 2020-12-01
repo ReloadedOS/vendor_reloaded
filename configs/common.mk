f63b7b5fb654080ec3adaf2358fc943dd3ab64c4
Alexander Koskovich <zvnexus@outlook.com>
2020-12-01 13:43:18 -0700

    configs: Enable blurs by default
     * Required for the blur option in Settings to work,
       if the user wants to turn off blur for performance
       reasons they can toggle.
    
    Change-Id: Id21aaba8187d211892da689a3cb622b801510343

--- configs/common.mk
+++ configs/common.mk
@@ -141,3 +141,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # WaveOS Widget
 PRODUCT_PACKAGES += \
     WaveWidget
+
+# Blurs
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    ro.sf.blurs_are_expensive=1 \
+    ro.surface_flinger.supports_background_blur=1
