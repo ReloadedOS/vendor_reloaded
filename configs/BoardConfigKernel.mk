PearlOS/platform_vendor_pearl

Move perl base modules to common prebuilts dir
Change-Id: I88df0825c26aaebfa9329cf15eb93d8e2bf728e1
Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

config/BoardConfigKernel.mk
@@ -132,7 +132,7 @@ endif
 TOOLS_PATH_OVERRIDE := \
     PATH=$(BUILD_TOP)/prebuilts/tools-extras/$(HOST_OS)-x86/bin:$$PATH \
     LD_LIBRARY_PATH=$(BUILD_TOP)/prebuilts/tools-extras/$(HOST_OS)-x86/lib:$$LD_LIBRARY_PATH \
-    PERL5LIB=$(BUILD_TOP)/prebuilts/tools-extras/$(HOST_OS)-x86/lib/perl-base
+    PERL5LIB=$(BUILD_TOP)/prebuilts/tools-extras/common/perl-base
 
 # Set DTBO image locations so the build system knows to build them
 ifeq ($(TARGET_NEEDS_DTBOIMAGE),true)

ReloadedOS/vendor_reloaded

configs/BoardConfigKernel.mk
@@ -131,7 +131,7 @@ endif
 TOOLS_PATH_OVERRIDE := \
     PATH=$(BUILD_TOP)/prebuilts/tools-wave/$(HOST_OS)-x86/bin:$$PATH \
     LD_LIBRARY_PATH=$(BUILD_TOP)/prebuilts/tools-wave/$(HOST_OS)-x86/lib:$$LD_LIBRARY_PATH \
-    PERL5LIB=$(BUILD_TOP)/prebuilts/tools-wave/$(HOST_OS)-x86/lib/perl-base
+    PERL5LIB=$(BUILD_TOP)/prebuilts/tools-wave/common/perl-base
 
 # Set DTBO image locations so the build system knows to build them
 ifeq ($(TARGET_NEEDS_DTBOIMAGE),true)