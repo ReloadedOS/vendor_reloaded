19beaa0b048b917ae01609a62c0120072d3f08d3
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-12-15 23:07:13 +0530

    configs: Don't build telephony-ext by default
     * we are no longer a CAF-based ROM

--- configs/common.mk
+++ configs/common.mk
@@ -78,10 +78,6 @@ PRODUCT_PACKAGES += \
     rcs_service_api \
     rcs_service_api.xml
 
-# MSIM manual provisioning
-PRODUCT_PACKAGES += telephony-ext
-PRODUCT_BOOT_JARS += telephony-ext
-
 # TCP Connection Management
 PRODUCT_PACKAGES += tcmiface
 PRODUCT_BOOT_JARS += tcmiface
