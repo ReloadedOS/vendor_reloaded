69992274a988dca7ec932dd24ef2bdddff5d79ca
Abhishek Aggarwal <warabhishek@gmail.com>
2021-02-04 18:46:25 +0000

    soong: Add flag for fod extension
    [Pig]: Convert to soong config variables
    Change-Id: Ib45706cc50fa88b9b05af1c68eec9d1f08d70569

--- configs/BoardConfigSoong.mk
+++ configs/BoardConfigSoong.mk
@@ -31,7 +31,8 @@ SOONG_CONFIG_waveGlobalVars += \
     additional_gralloc_10_usage_bits \
     target_init_vendor_lib \
     target_ld_shim_libs \
-    target_process_sdk_version_override
+    target_process_sdk_version_override \
+    target_surfaceflinger_fod_lib
 
 SOONG_CONFIG_NAMESPACES += waveQcomVars
 SOONG_CONFIG_waveQcomVars += \
@@ -55,12 +56,14 @@ SOONG_CONFIG_waveQcomVars_needs_camera_boottime_timestamp := $(TARGET_CAMERA_BOO
 # Set default values
 TARGET_INIT_VENDOR_LIB ?= vendor_init
 TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS ?= 0
+TARGET_SURFACEFLINGER_FOD_LIB ?= surfaceflinger_fod_lib
 
 # Soong value variables
 SOONG_CONFIG_waveGlobalVars_additional_gralloc_10_usage_bits := $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS)
 SOONG_CONFIG_waveGlobalVars_target_init_vendor_lib := $(TARGET_INIT_VENDOR_LIB)
 SOONG_CONFIG_waveGlobalVars_target_ld_shim_libs := $(subst $(space),:,$(TARGET_LD_SHIM_LIBS))
 SOONG_CONFIG_waveGlobalVars_target_process_sdk_version_override := $(TARGET_PROCESS_SDK_VERSION_OVERRIDE)
+SOONG_CONFIG_waveGlobalVars_target_surfaceflinger_fod_lib := $(TARGET_SURFACEFLINGER_FOD_LIB)
 ifneq ($(filter $(QSSI_SUPPORTED_PLATFORMS),$(TARGET_BOARD_PLATFORM)),)
 SOONG_CONFIG_waveQcomVars_qcom_display_headers_namespace := vendor/qcom/opensource/commonsys-intf/display
 else
