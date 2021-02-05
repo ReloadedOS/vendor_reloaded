89880b84871267e0da25e0c9ebb202a9a7aa9d79
Luk1337 <priv.luk@gmail.com>
2021-02-05 10:40:13 +0000

    vendor: sepolicy: Add rules for FOD

--- configs/BoardConfigWave.mk
+++ configs/BoardConfigWave.mk
@@ -6,5 +6,12 @@ ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 include vendor/wave/configs/BoardConfigQcom.mk
 endif
 
+# SEPolicy
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    vendor/wave/sepolicy/private
+
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+    vendor/wave/sepolicy/public
+
 # Soong
 include vendor/wave/configs/BoardConfigSoong.mk
