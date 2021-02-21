12670ee4d8b8ab088c05aa2f1ede496f4b521719
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-02-21 15:12:50 +0530

    sepolicy: Move FOD to dynamic sepolicy
     * currently its forced to private sepolicy, allow it to
       be used by oss vendor devices as well, like LOS

--- configs/BoardConfigWave.mk
+++ configs/BoardConfigWave.mk
@@ -7,8 +7,19 @@ include vendor/wave/configs/BoardConfigQcom.mk
 endif
 
 # SEPolicy
+ifeq ($(TARGET_COPY_OUT_VENDOR), vendor)
+ifeq ($(BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE),)
+TARGET_USES_PREBUILT_VENDOR_SEPOLICY ?= true
+endif
+endif
+
+ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    vendor/wave/sepolicy/private
+    vendor/wave/sepolicy/dynamic
+else
+BOARD_VENDOR_SEPOLICY_DIRS += \
+    vendor/wave/sepolicy/dynamic
+endif
 
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
     vendor/wave/sepolicy/public
