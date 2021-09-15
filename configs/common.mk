30faaf76628772ae252f627a7cdd9bb85c903417
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-09-15 21:48:32 +0530

    rro_overlays: Add Google Sans / Roboto font

--- configs/common.mk
+++ configs/common.mk
@@ -155,6 +155,7 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PACKAGES += \
     FontGoogleSansLatoOverlay \
+    FontGoogleSansRobotoOverlay \
     FontInterOverlay \
     FontManropeOverlay \
     FontOnePlusSansOverlay
