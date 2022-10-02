0e18bcbf22198fd31b86e779736ef34dda8b7662
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-10-03 00:03:12 +0530

    build: Clean up soong configuration

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -28,50 +28,21 @@ $(foreach v,$(EXPORT_TO_SOONG),$(eval $(call addVar,$(v))))
 
 SOONG_CONFIG_NAMESPACES += reloadedGlobalVars
 SOONG_CONFIG_reloadedGlobalVars += \
-    additional_gralloc_10_usage_bits \
     target_init_vendor_lib \
     target_ld_shim_libs \
     target_process_sdk_version_override \
-    target_surfaceflinger_fod_lib
-
-SOONG_CONFIG_NAMESPACES += reloadedQcomVars
-SOONG_CONFIG_reloadedQcomVars += \
-    legacy_hw_disk_encryption \
-    supports_extended_compress_format \
+    target_surfaceflinger_fod_lib \
     uses_pre_uplink_features_netmgrd \
-    uses_qti_camera_device \
     needs_camera_boottime_timestamp
-
-# Only create display_headers_namespace var if dealing with UM platforms to avoid breaking build for all other platforms
-ifneq ($(filter $(UM_PLATFORMS),$(TARGET_BOARD_PLATFORM)),)
-SOONG_CONFIG_reloadedQcomVars += \
-    qcom_display_headers_namespace
-endif
-
-# Soong bool variables
-SOONG_CONFIG_reloadedQcomVars_legacy_hw_disk_encryption := $(TARGET_LEGACY_HW_DISK_ENCRYPTION)
-SOONG_CONFIG_reloadedQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
-SOONG_CONFIG_reloadedQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
-SOONG_CONFIG_reloadedQcomVars_uses_qti_camera_device := $(TARGET_USES_QTI_CAMERA_DEVICE)
-SOONG_CONFIG_reloadedQcomVars_needs_camera_boottime_timestamp := $(TARGET_CAMERA_BOOTTIME_TIMESTAMP)
 
 # Set default values
 TARGET_INIT_VENDOR_LIB ?= vendor_init
-TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS ?= 0
 TARGET_SURFACEFLINGER_FOD_LIB ?= surfaceflinger_fod_lib
 
 # Soong value variables
-SOONG_CONFIG_reloadedGlobalVars_additional_gralloc_10_usage_bits := $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS)
+SOONG_CONFIG_reloadedGlobalVars_needs_camera_boottime_timestamp := $(TARGET_CAMERA_BOOTTIME_TIMESTAMP)
 SOONG_CONFIG_reloadedGlobalVars_target_init_vendor_lib := $(TARGET_INIT_VENDOR_LIB)
 SOONG_CONFIG_reloadedGlobalVars_target_ld_shim_libs := $(subst $(space),:,$(TARGET_LD_SHIM_LIBS))
 SOONG_CONFIG_reloadedGlobalVars_target_process_sdk_version_override := $(TARGET_PROCESS_SDK_VERSION_OVERRIDE)
 SOONG_CONFIG_reloadedGlobalVars_target_surfaceflinger_fod_lib := $(TARGET_SURFACEFLINGER_FOD_LIB)
-ifneq ($(filter $(QSSI_SUPPORTED_PLATFORMS),$(TARGET_BOARD_PLATFORM)),)
-SOONG_CONFIG_reloadedQcomVars_qcom_display_headers_namespace := vendor/qcom/opensource/commonsys-intf/display
-else
-SOONG_CONFIG_reloadedQcomVars_qcom_display_headers_namespace := $(QCOM_SOONG_NAMESPACE)/display
-endif
-
-ifneq ($(TARGET_USE_QTI_BT_STACK),true)
-PRODUCT_SOONG_NAMESPACES += packages/apps/Bluetooth
-endif #TARGET_USE_QTI_BT_STACK
+SOONG_CONFIG_reloadedGlobalVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
