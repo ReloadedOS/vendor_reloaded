2dd619c08729e0d5ac537198041893a3b2111794
BladeRunner-A2C <john.smith@unused.email>
2023-07-21 15:21:53 +0600

    config: Update CLO revision to LA.QSSI.13.0.r1-10900.01-qssi.0
    Signed-off-by: BladeRunner-A2C <john.smith@unused.email>

--- config/branding.mk
+++ config/branding.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-CLO_REVISION := LA.QSSI.13.0.r1-10700-qssi.0
+CLO_REVISION := LA.QSSI.13.0.r1-10900.01-qssi.0
 
 RELOADED_VERSION_BASE := 13.0
 RELOADED_BUILD_DATE := $(shell date +%Y%m%d)
